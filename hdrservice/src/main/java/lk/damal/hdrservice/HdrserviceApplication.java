b0a6480a5c614508c8d9a3e97d01d9fa843412c6
Damal Ranula <ranuladamal@gmail.com>
2024-06-03 23:52:22 +0530

    Add files via upload
    Create the Springboot project

---
+++ hdrservice/src/main/java/lk/damal/hdrservice/HdrserviceApplication.java
@@ -0,0 +1,14 @@
+package lk.damal.hdrservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class HdrserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdrserviceApplication.class, args);
+	}
+
+}
---
+++ hdrservice/src/test/java/lk/damal/hdrservice/HdrserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.damal.hdrservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdrserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
